ebfe236eb2aab4d6ae183564b8216d4d2ac06404
Preetam <vpulla@asu.edu>
2026-05-25 12:54:56 -0700

    MlClientService is mocked

--- src/test/java/com/example/resume/ResumeApplicationTests.java
+++ src/test/java/com/example/resume/ResumeApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.resume;
 
+import com.example.resume.ml.MlClientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ResumeApplicationTests {
+
+	@MockitoBean
+	private MlClientService mlClientService;
 
 	@Test
 	void contextLoads() {
